035ead96a5734340b6113cd625c0f16642433808
Nethelbert <gilblasse@gmail.com>
2020-02-20 15:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let pTag = document.getElementById("text");
   pTag.innerText = "This is really cool!"
 });
+ 
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
