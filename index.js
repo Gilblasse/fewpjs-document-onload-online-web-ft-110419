801ee7545f7f972f40551038b38b58e00d25940f
Nethelbert <gilblasse@gmail.com>
2020-02-20 15:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   let pTag = document.getElementById("text");
-  pTag.innerHTML = "This is really cool!"
+  pTag.innerHTML = "This is really cool!";
 });
